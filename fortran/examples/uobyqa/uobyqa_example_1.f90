c36f38cbc969c3747284fcdc351f0b4956999440
jinangshah21 <jinang2110@gmail.com>
2025-01-11 12:01:10 +0530

    Test: uncomment add test for `uobyqa_example_1`

--- fortran/examples/uobyqa/uobyqa_example_1.f90
+++ fortran/examples/uobyqa/uobyqa_example_1.f90
@@ -59,29 +59,26 @@ end module calfun_mod
 !---------------------------------------- THE MAIN PROGRAM ----------------------------------------!
 program uobyqa_exmp
 
-! ! The following line makes the solver available.
-! use uobyqa_mod, only : uobyqa
+ ! The following line makes the solver available.
+ use uobyqa_mod, only : uobyqa
+ ! The following line specifies which module provides CALFUN and CALLBACK_FCN.
+ use calfun_mod, only : RP, IK, calfun, callback_fcn
+ implicit none
+ integer, parameter :: n = 2
+ integer :: nf, info
+ real(RP) :: f, x(n), x0(n)
+ ! Define the starting point.
+ x0 = 0.0_RP
+ ! The following lines illustrates how to call the solver.
+ x = x0
+ call uobyqa(calfun, x, f)  ! This call will not print anything.
+ ! In addition to the compulsory arguments, the following illustration specifies also RHOBEG and
+ ! IPRINT, which are optional. All the unspecified optional arguments (RHOEND, MAXFUN, etc.) will
+ ! take their default values coded in the solver.
+ x = x0
+ call uobyqa(calfun, x, f, rhobeg=1.0_RP, iprint=1_IK, nf=nf, info=info, callback_fcn=callback_fcn)
 
-! ! The following line specifies which module provides CALFUN and CALLBACK_FCN.
-! use calfun_mod, only : RP, IK, calfun, callback_fcn
-
-! implicit none
-
-! integer, parameter :: n = 2
-! integer :: nf, info
-! real(RP) :: f, x(n), x0(n)
-
-! ! Define the starting point.
-! x0 = 0.0_RP
-
-! ! The following lines illustrates how to call the solver.
-! x = x0
-! call uobyqa(calfun, x, f)  ! This call will not print anything.
-
-! ! In addition to the compulsory arguments, the following illustration specifies also RHOBEG and
-! ! IPRINT, which are optional. All the unspecified optional arguments (RHOEND, MAXFUN, etc.) will
-! ! take their default values coded in the solver.
-! x = x0
-! call uobyqa(calfun, x, f, rhobeg=1.0_RP, iprint=1_IK, nf=nf, info=info, callback_fcn=callback_fcn)
-
+if(nf /= 21) error stop
+if (any(abs(x - [5.0_RP, 4.0_RP]) > 1.0e-12_RP)) error stop
+if (abs(f - 0.0_RP) > 1.0e-12_RP)  error stop
 end program uobyqa_exmp
